85251dfcfe543d05b762e76769ee70fedff3f691
Magdalena Jankowska/Magda <magda.desirec@gmail.com>
2019-05-12 11:41:17 +0100

    fibonacci while

--- src/main/java/com/sda/algorytmy/App.java
+++ src/main/java/com/sda/algorytmy/App.java
@@ -1,5 +1,7 @@
 package com.sda.algorytmy;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -13,8 +15,31 @@ public class App
             return fibonacci(n-2) + fibonacci(n-1);
         }
     }
+    public static void fibonacci2(){
+
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        if (N < 2) {
+            System.out.println(N);
+        }else{
+            int A = 0;
+            int B = 1;
+            int C = 1;
+            int K = 2;
+
+            while (K < N){
+                A = B;
+                B = C;
+                C = A + B;
+                K ++;
+            }
+            System.out.println(C);
+        }
+
+    }
     public static void main( String[] args )
     {
+        fibonacci2();
         System.out.println(fibonacci(5));
       int tab[] = new int [20];
       tab [0] = 0;
